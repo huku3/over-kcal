ecee00dde78eb9eb55d5c407284a5899158a5981
huku3 <mhideyuki7777@icloud.com>
2022-11-17 21:23:08 +0900

    a

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from PIL import Image
 import pyocr
+import csv
+import glob
+import pyocr.builders
+
 
 tools = pyocr.get_available_tools()
 tool = tools[0]
@@ -8,10 +12,33 @@ tool = tools[0]
 
 
 # img01 = Image.open("images/01.png", "images/02.png")
-img02 = Image.open("images/02.png")
-img02.show()
+# img = Image.open("images/05.png")
+# img02.show()
 
 
-txt = tool.image_to_string(img02, lang="eng+jpn", builder=pyocr.builders.TextBuilder())
+def image_to_text(file_path):
+    txt = tool.image_to_string(
+        Image.open(file_path),  # OCRする画像
+        lang="eng",  # 学習済み言語データ
+        builder=pyocr.builders.TextBuilder(),  # 期待される出力のタイプを指定
+    )
 
-print(txt)
+    return txt
+
+
+def main():
+    file_paths = glob.glob("images/*")
+
+    for file_path in file_paths:
+        txt = image_to_text(file_path)
+
+    print(txt)
+    with open("sample.txt", mode="w") as f:
+        f.writelines(txt)
+
+
+# print(f.read())
+# f.close()
+
+if __name__ == "__main__":
+    main()
